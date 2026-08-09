a1deb35f91c404bb05861c0cac15b564f89d2a39
Fire220Creep <emersonortiz01@gmail.com>
2026-08-09 00:27:47 -0400

    Delete scripts directory (oops silly me)
    wrong upload path xd

--- scripts/pages/aboutOC-local.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    const lang = localStorage.getItem('siteLanguage') === 'en' ? 'en' : 'es';
-    document.title = lang === 'en' ? "Sulfur's Archives - Keeby" : "Archivos de Sulfur - Keeby";
-};
--- scripts/pages/blog-main.js
+++
@@ -1,8 +0,0 @@
-function closeWindow(){
-    const redZone = document.getElementById('redZone');
-    if (redZone.style.display != 'none') {
-        redZone.style.display = 'none';
-    }
-    else{
-        console.log("???");}
-}
